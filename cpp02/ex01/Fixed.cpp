86e3dfaf2d676912b5b089674e25d925b13239c8
duarteagostinho <duarteaagostinho@gmail.com>
2026-01-19 13:49:40 +0000

    some cpp files

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -1,34 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: duandrad <duandrad@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/06 14:00:05 by duandrad          #+#    #+#             */
-/*   Updated: 2025/08/25 10:43:58 by duandrad         ###   ########.fr       */
+/*   Created: 2025/12/22 17:49:34 by duandrad          #+#    #+#             */
+/*   Updated: 2025/12/22 18:35:06 by duandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIE_HPP
-#define ZOMBIE_HPP
+#include "Fixed.hpp"
 
-#include <iostream>
+int		Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->value;
+}
 
-class Zombie {
+void	Fixed::setRawBits(int const raw) {
 	
-	std::string name;
-
-	public:
-		void		setName(std::string Name);
-		std::string getName();
-		void		announce();
-	Zombie() {}
-	~Zombie() {
-		std::cout << name << " has been destroyed" << std::endl;
-	}
-};
-
-Zombie* zombieHorde(int N, std::string name);
-
-#endif //ZOMBIE_HPP
+	this->value = raw;
+}
