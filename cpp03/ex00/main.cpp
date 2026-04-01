d995764176f79fdb19e981dddd88aaa251ba2ba0
duarteagostinho <duarteaagostinho@gmail.com>
2026-04-01 17:18:47 +0100

    Add megaphone example, remove PhoneBook, and update main.cpp with comments and fixes

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -4,16 +4,22 @@ int main()
 {
 	ClapTrap ct("BOBBY");
 
-	std::cout << ct.getName() << " has " << ct.getHitPoints() << " hit points and " << ct.getEnergyPoints() << " energy points" << std::endl;
+	// Initial state
+	std::cout << ct.getName() << " has " << ct.getHitPoints() << " hit points and "
+		<< ct.getEnergyPoints() << " energy points" << std::endl;
 
+	// Attacks using energy points
 	ct.attack("training_dummy");
 	ct.attack("target_dummy");
 
+	// Take some damage
 	ct.takeDamage(3);
 
+	// Repair until energy runs out (more than 10 times)
 	for (int i = 0; i < 12; i++)
 		ct.beRepaired(1);
 
+	// Make ClapTrap reach 0 hit points, then try actions
 	ct.takeDamage(100);
 	ct.attack("after_death_dummy");
 	ct.beRepaired(1);
